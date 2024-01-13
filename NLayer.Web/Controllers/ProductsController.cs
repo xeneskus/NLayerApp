59a9a95eb977bb595e2785f322abea318d871947
xeneskus <xeneskus@gmail.com>
2024-01-13 18:56:25 +0300

    Products controller update and remove added

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -58,5 +58,49 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name");
             return View();
         }
+       
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+
+
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name", product.CategoryId);
+
+            return View(_mapper.Map<ProductDto>(product));
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+
+                await _services.UpdateAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+
+        }
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+
+
+            await _services.RemoveAsync(product);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
