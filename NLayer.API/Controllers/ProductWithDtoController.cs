b571fa90a18d89dba546be6d77378f212a512026
xeneskus <xeneskus@gmail.com>
2024-01-22 08:26:41 +0300

    ProductWithDtoController added.

---
+++ NLayer.API/Controllers/ProductWithDtoController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductWithDtoController : CustomBaseController
+    {
+        private readonly IProductServiceWithDto _productServiceWithDto;
+
+        public ProductWithDtoController(IProductServiceWithDto productServiceWithDto)
+        {
+            _productServiceWithDto = productServiceWithDto;
+        }
+
+
+        //products/GetProductsWithCategory
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+            return CreateActionResult(await _productServiceWithDto.GetProductsWithCategory());
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+
+            return CreateActionResult(await _productServiceWithDto.GetAllAsync());
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))] //filter miras farkından dolayı 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+            return CreateActionResult(await _productServiceWithDto.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductCreateDto productDto)
+        {
+
+            return CreateActionResult(await _productServiceWithDto.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+
+            return CreateActionResult(await _productServiceWithDto.UpdateAsync(productDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+      
+            return CreateActionResult(await _productServiceWithDto.RemoveAsync(id));
+        }
+    }
+
+}
